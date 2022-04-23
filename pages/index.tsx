65ffa5bfa91fc0e71ddff743416ee122072dbfca
김한솔 <kimhan0421@naver.com>
2022-04-23 11:58:32 +0900

    Feat: filter layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,33 @@
 import type { NextPage } from 'next';
 
-import { Layout, Tag } from 'components/Atoms';
+import ArrowDown from 'assets/list/ArrowDown';
+import { Box, FlexBox, Layout, Tag } from 'components/Atoms';
+import ListFilter from 'components/Molecules/ListFilter';
 import { useInitHeader } from 'utils/hooks/useInitHeader';
 
 const Home: NextPage = () => {
   useInitHeader({ headerLeft: 'logo', headerEnd: 'home' });
   return (
-    <Layout>
-      <Tag backgroundColor="#000" color="#ceff00">
-        전시회
-      </Tag>
-      <Tag backgroundColor="#eee" color="#000">
-        D-63
-      </Tag>
-    </Layout>
+    <>
+      <FlexBox>
+        <Box flex={1}>
+          <ListFilter title="조회순" icon={<ArrowDown />} />
+        </Box>
+        <Box flex={1}>
+          <ListFilter title="조회순" icon={<ArrowDown />} />
+        </Box>
+      </FlexBox>
+      <Layout>
+        <Box margin="14px 0 10px">
+          <Tag backgroundColor="#000" color="#ceff00">
+            전시회
+          </Tag>
+          <Tag backgroundColor="#eee" color="#000">
+            D-63
+          </Tag>
+        </Box>
+      </Layout>
+    </>
   );
 };
 
