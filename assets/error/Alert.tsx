a8d8585a26bb49d6a0103fffae8eb2007cc14b07
김민혜 <kmhkgh@naver.com>
2022-04-23 13:15:49 +0900

    Feat: error page layout fix

--- assets/error/Alert.tsx
+++ assets/error/Alert.tsx
@@ -1,9 +1,9 @@
-export default function Alert() {
+export default function Alert({ width = '50', height = '50' }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
+      width={width}
+      height={height}
       viewBox="0 0 50 50"
     >
       <g data-name="*alert_50*50">
