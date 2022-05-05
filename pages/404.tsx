f8d3506eb3bcfd7636ce7b9a1ec35b5df7fe94ab
noahlee <azxca1731@gmail.com>
2022-05-06 00:34:25 +0900

    ready to deploy

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,5 @@
-import { useUserProps } from 'utils/authentication/useUser';
 import { useInitHeader } from 'utils/hooks/useInitHeader';
 
-export const getServerSideProps = useUserProps;
 export default function Error404() {
   useInitHeader({ headerLeft: 'disabled', headerRight: 'disabled' });
   return <div>에러페이지</div>;
