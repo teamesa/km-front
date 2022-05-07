ab815a38dd57878fa8dd0c09c679e5def676a292
김한솔 <kimhan0421@naver.com>
2022-05-07 14:14:02 +0900

    Fix: conflit error

--- components/Organisms/Common/Container.tsx
+++ components/Organisms/Common/Container.tsx
@@ -1,22 +1,11 @@
-<<<<<<< HEAD:components/Organisms/common/Container.tsx
-import NavHome from 'assets/commom/bottomTabNavigator/NavHome';
-import NavList from 'assets/commom/bottomTabNavigator/NavList';
-import NavMyPage from 'assets/commom/bottomTabNavigator/NavMyPage';
-import NavWish from 'assets/commom/bottomTabNavigator/NavWish';
+import NavHome from 'assets/common/bottomTabNavigator/NavHome';
+import NavList from 'assets/common/bottomTabNavigator/NavList';
+import NavMyPage from 'assets/common/bottomTabNavigator/NavMyPage';
+import NavWish from 'assets/common/bottomTabNavigator/NavWish';
 import { Box } from 'components/Atoms';
 import BottomTabNavigator from 'components/Organisms/Common/BottomTabNavigator';
 import HeaderBar from 'components/Organisms/Common/HeaderBar';
 import theme from 'styles/theme';
-=======
-import NavHome from 'assets/common/bottomTabNavigator/NavHome';
-import NavList from 'assets/common/bottomTabNavigator/NavList';
-import NavMap from 'assets/common/bottomTabNavigator/NavMap';
-import NavMyPage from 'assets/common/bottomTabNavigator/NavMyPage';
-import NavWish from 'assets/common/bottomTabNavigator/NavWish';
-import { Box } from 'components/Atoms';
-import BottomTabNavigator from 'components/Organisms/common/BottomTabNavigator';
-import HeaderBar from 'components/Organisms/common/HeaderBar';
->>>>>>> main:components/Organisms/Common/Container.tsx
 
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
