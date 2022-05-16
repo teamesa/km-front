9d783353c129f82d7f0f37f71d2b7d41ead88a8b
mndangee <38105502+mndangee@users.noreply.github.com>
2022-05-16 21:11:31 +0900

    Feat: error layout (#8)

---
+++ assets/error/Alert.tsx
@@ -0,0 +1,39 @@
+export default function Alert({ width = '50', height = '50' }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 50 50"
+    >
+      <g data-name="*alert_50*50">
+        <g
+          data-name="타원 227"
+          fill="#fff"
+          stroke="#000"
+          strokeMiterlimit="10"
+          strokeWidth="2px"
+        >
+          <circle cx="25" cy="25" r="25" stroke="none" />
+          <circle cx="25" cy="25" r="24" fill="none" />
+        </g>
+        <g data-name="그룹 1156" transform="translate(23 12.219)">
+          <rect
+            data-name="사각형 1672"
+            width="3.375"
+            height="18.75"
+            rx="1.687"
+            transform="translate(.312)"
+          />
+          <circle
+            data-name="타원 228"
+            cx="2"
+            cy="2"
+            r="2"
+            transform="translate(0 21.562)"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
