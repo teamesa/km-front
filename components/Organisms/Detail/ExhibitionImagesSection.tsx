846aa5024c90c5f6341389764118beb040ea0283
김한솔 <kimhan0421@naver.com>
2022-06-05 02:08:54 +0900

    Refactor: detail url  수정

--- components/Organisms/Detail/ExhibitionImagesSection.tsx
+++ components/Organisms/Detail/ExhibitionImagesSection.tsx
@@ -2,5 +2,5 @@ import { Box } from 'components/Atoms';
 import theme from 'styles/theme';
 
 export default function ExhibitionImagesSection() {
-  return <Box width="100%" height="500px" background={theme.colors.lime} />;
+  return <Box width="100%" height="300px" background={theme.colors.lime} />;
 }
