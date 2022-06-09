f930ec510a4c7017812932526ea8c6a55a640a09
noahlee <azxca1731@gmail.com>
2022-06-10 02:51:43 +0900

    show userProps value

--- components/Organisms/MyPage/UpdateHome.tsx
+++ components/Organisms/MyPage/UpdateHome.tsx
@@ -1,17 +1,27 @@
 import { useRouter } from 'next/router';
 
-import { Box, Input, RadioInput, RadioLabel, Span } from 'components/Atoms';
+import { Box, Input, RadioInput, RadioLabel } from 'components/Atoms';
 import theme from 'styles/theme';
 import { UserProps } from 'utils/authentication/useUser';
 import { useInitHeader } from 'utils/hooks/useInitHeader';
 
-export default function UpdateHome({ user }: UserProps) {
+export default function UpdateHome({
+  user: { name, phoneNumber, birthdate, gender, email },
+}: UserProps) {
   const router = useRouter();
   useInitHeader({
     headerLeft: 'default',
     headerRight: 'search',
     title: '회원정보 수정',
   });
+
+  const birthDayFormat = new Date(birthdate)
+    .toISOString()
+    .substring(0, 10)
+    .replaceAll('-', '');
+  const phoneNumberFormat = phoneNumber.replaceAll('-', '');
+  const genderFormat = gender !== 'MALE';
+
   return (
     <Box paddingLeft={'15px'} paddingRight={'15px'}>
       <Box marginTop="40px" width="100%">
@@ -28,7 +38,7 @@ export default function UpdateHome({ user }: UserProps) {
         >
           닉네임
         </Box>
-        <Input placeholder="입력해주세요" />
+        <Input placeholder="입력해주세요" value={name} />
       </Box>
       <Box marginTop="30px" width="100%">
         <Box
@@ -44,7 +54,11 @@ export default function UpdateHome({ user }: UserProps) {
         >
           연락처
         </Box>
-        <Input placeholder="입력해주세요" />
+        <Input
+          placeholder="입력해주세요"
+          value={phoneNumberFormat}
+          type="number"
+        />
       </Box>
       <Box marginTop="30px" width="100%">
         <Box
@@ -60,7 +74,12 @@ export default function UpdateHome({ user }: UserProps) {
         >
           생일
         </Box>
-        <Input placeholder="입력해주세요" type="number" />
+        <Input
+          placeholder="입력해주세요"
+          type="number"
+          value={birthDayFormat}
+          maxLength={8}
+        />
       </Box>
       <Box marginTop="30px" width="100%">
         <Box
@@ -77,9 +96,19 @@ export default function UpdateHome({ user }: UserProps) {
           성별
         </Box>
         <Box>
-          <RadioInput type="radio" name="gender" id="female" />
+          <RadioInput
+            type="radio"
+            name="gender"
+            id="female"
+            checked={genderFormat}
+          />
           <RadioLabel htmlFor="female">여성</RadioLabel>
-          <RadioInput type="radio" name="gender" id="male" />
+          <RadioInput
+            type="radio"
+            name="gender"
+            id="male"
+            checked={!genderFormat}
+          />
           <RadioLabel htmlFor="male">남성</RadioLabel>
         </Box>
       </Box>
@@ -97,7 +126,7 @@ export default function UpdateHome({ user }: UserProps) {
         >
           이메일
         </Box>
-        <Input placeholder="입력해주세요" type="email" />
+        <Input placeholder="입력해주세요" type="email" value={email} />
       </Box>
       <Box
         marginTop="60px"
