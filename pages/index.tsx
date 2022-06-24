8e3f95c2f3fcc060fcc200f4b4824c7a4eb8f4ce
noahlee <azxca1731@gmail.com>
2022-06-24 22:38:02 +0900

    remove error phrase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,15 @@
 import type { NextPage } from 'next';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import ArrowDown from 'assets/list/ArrowDown';
 import { Box, FlexBox, Layout, Tag } from 'components/Atoms';
 import Select from 'components/Atoms/Select';
 import ListFilter from 'components/Molecules/ListFilter';
 import ListCategory from 'components/Organisms/List/ListCategory';
-import { ResponseState, TestSate } from 'states';
 import theme from 'styles/theme';
-import customAxios from 'utils/hooks/customAxios';
 import { useInitHeader } from 'utils/hooks/useInitHeader';
 
 const Home: NextPage = () => {
   useInitHeader({ headerLeft: 'logo', headerEnd: 'home' });
-  const data = useRecoilValue(TestSate);
-  const setResponseState = useSetRecoilState(ResponseState);
-
-  /** POST로 보내는 예시 */
-  const handleTest = async (e: any) => {
-    e.preventDefault();
-
-    const data = 'test';
-    const axios = customAxios();
-    try {
-      const response = await axios({
-        url: '/hello-example',
-        method: 'POST',
-        data,
-      });
-      console.log('response', response);
-      setResponseState(response);
-    } catch (e: any) {
-      console.log('error', e.response);
-    }
-  };
   // 정렬레이어 팝업
   const exampleSelect = [
     { index: 0, label: 'test1', value: '010' },
@@ -74,9 +50,6 @@ const Home: NextPage = () => {
             D-63
           </Tag>
         </Box>
-        <Box>상태값 가져오기</Box>
-        <Box>Post로 memo보내기</Box>
-        <button onClick={handleTest}>테스트</button>
         <hr />
         <Select
           modalType="Select"
