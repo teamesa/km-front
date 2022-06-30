3b6bb5ce08ff521fd9f15b7333a430e07ad9637a
김한솔 <kimhan0421@naver.com>
2022-07-01 04:17:50 +0900

    Feat: font weight

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -12,6 +12,18 @@ export default function GlobalStyles() {
   return (
     <Global
       styles={css`
+        body {
+          font-family: -apple-system, SpoqaHanSansNeo, sans-serif;
+        }
+        @font-face {
+          font-family: 'SpoqaHanSansNeo';
+          src: url('/fonts/SpoqaHanSansNeo-Light.ttf') format('ttf'),
+            url('/fonts/SpoqaHanSansNeo-Medium.ttf') format('ttf'),
+            url('/fonts/SpoqaHanSansNeo-Bold.ttf') format('ttf'),
+            url('/fonts/SpoqaHanSansNeo-Regular.ttf') format('ttf'),
+            url('/fonts/SpoqaHanSansNeo-Thin.ttf') format('ttf');
+        }
+
         :root {
           --platformPadding: ${isiOS ? '24px' : '16px'};
           --platformBottomArea: ${isiOS ? '24px' : '0px'};
