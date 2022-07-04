8bcc3de51014643cced34a3dfc2aff7f994bf1b2
김민혜 <kmhkgh@naver.com>
2022-07-04 15:27:36 +0900

    Feat: 정령 팝업 API연결

--- components/Molecules/ListFilter.tsx
+++ components/Molecules/ListFilter.tsx
@@ -5,7 +5,7 @@ import ListSortFilter from 'components/Organisms/List/ListSortFilter';
 const sortType = [
   { index: 0, label: '등록순', value: 'ENROLL_DESC' },
   { index: 1, label: '종료임박순', value: 'END_DATE_ASC' },
-  { index: 2, label: '하트pick순', value: 'HEART_DESC' },
+  { index: 2, label: '하트PICK순', value: 'HEART_DESC' },
   { index: 2, label: '별점순', value: 'GRADE_DESC' },
 ];
 
@@ -32,28 +32,4 @@ export default function ListFilter() {
       </Box>
     </>
   );
-  // return title == '상세필터' ? (
-  //   <FlexBox
-  //     padding="0px 20px"
-  //     lineHeight="38px"
-  //     justifyContent="space-between"
-  //     border={`1px solid ${theme.colors.grayDD}`}
-  //     borderLeft="none"
-  //     fontSize="12px"
-  //   >
-  //     {title}
-  //     <Button>{icon}</Button>
-  //   </FlexBox>
-  // ) : (
-  //   <FlexBox
-  //     padding="0px 20px"
-  //     lineHeight="38px"
-  //     justifyContent="space-between"
-  //     border={`1px solid ${theme.colors.grayDD}`}
-  //     fontSize="12px"
-  //   >
-  //     {title}
-  //     <Button>{icon}</Button>
-  //   </FlexBox>
-  // );
 }
--- components/Organisms/List/ListSortFilter.tsx
+++ components/Organisms/List/ListSortFilter.tsx
@@ -1,11 +1,28 @@
+import { useRecoilValue } from 'recoil';
+
 import ArrowDown from 'assets/list/ArrowDown';
 import { Box, Button } from 'components/Atoms';
 import { SelectProps } from 'constants/type/modal';
+import { searchRequest } from 'states/filter';
 import theme from 'styles/theme';
 import { useModal } from 'utils/hooks/useModal';
 
-export default function ListSortFilter(props: SelectProps) {
+interface sortTextProps {
+  sortText: string;
+}
+
+export default function ListSortFilter(
+  props: SelectProps,
+  { sortText }: sortTextProps,
+) {
   const { onModal } = useModal();
+  const targetText = useRecoilValue(searchRequest);
+  const getSortText = props.data.map((item) => {
+    if (targetText.searchSortType === item.value) {
+      sortText = item.label;
+      return sortText;
+    }
+  });
   return (
     <Button
       type="button"
@@ -25,7 +42,7 @@ export default function ListSortFilter(props: SelectProps) {
         });
       }}
     >
-      조회순
+      {getSortText}
       <Box position="absolute" top="0" right="20px">
         <ArrowDown />
       </Box>
