1ff618b787ebfcd2f9adca18d4c99fa5c6e23bb9
김한솔 <kimhan0421@naver.com>
2022-07-16 10:52:12 +0900

    Refactor: archive-api 수정

--- states/archiveWirte.ts
+++ states/archiveWirte.ts
@@ -1,4 +1,7 @@
-import { atom } from 'recoil';
+import { AxiosResponse } from 'axios';
+import { atom, selector, selectorFamily } from 'recoil';
+
+import customAxios from 'utils/hooks/customAxios';
 
 export interface ArchiveWirteProps {
   itemId: number;
@@ -13,6 +16,49 @@ export interface ArchiveWirteProps {
   }[];
   visibleAtItem: boolean;
 }
+
+interface TGetArchiveSearch {
+  contents: {
+    id: number;
+    link: string;
+    searchedTextLocationEnd: number;
+    searchedTextLocationStart: number;
+    title: string;
+  }[];
+  responsePagingStatus: {
+    currentContentsCount: number;
+    currentPage: number;
+    hasNext: boolean;
+    nextPage: number;
+    pageSize: number;
+    query: string;
+    totalContentsCount: number;
+  };
+}
+
+const axios = customAxios();
+
+export async function getArchiveSearch({ query }: { query: string }) {
+  const { data } = (await axios({
+    url: `/api/search/auto-complete`,
+    method: 'GET',
+    params: {
+      query,
+    },
+  })) as AxiosResponse<TGetArchiveSearch>;
+
+  return data;
+}
+
+export const ArchiveSearchState = selectorFamily({
+  key: 'ArchiveSearchState',
+  get: (query: string) => async () => {
+    const search = await getArchiveSearch({ query: query });
+    return {
+      search,
+    };
+  },
+});
 
 export const ArchiveWirteState = atom<ArchiveWirteProps | undefined>({
   key: 'ArchiveWirteState',
