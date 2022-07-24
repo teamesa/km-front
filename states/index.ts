f38f9d750fb82d9ab591297487af7dc053811e0b
Kim Han Sol <48988891+kimhan0421@users.noreply.github.com>
2022-07-24 22:17:02 +0900

    popup창 띄우기 (#66)
    * Feat: popup layout
    
    * Feat: popup 예시
    
    * Refactor: 확인팝업 띄우기
    
    * Refactor: ALERT_CONFIRM
    
    * Feat: 취소/확인 팝업

--- states/index.ts
+++ states/index.ts
@@ -4,8 +4,9 @@ import { DetailState } from './detail';
 import ListState from './list';
 import MyArchiveListState from './myArchiveList';
 import PopupNameState from './popupName';
-import { searchRequest } from './search-request';
 import TestSate from './test';
+
+import { searchRequest } from 'states/search-request';
 
 export {
   TestSate,
