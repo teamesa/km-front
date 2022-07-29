954ee3fb606f3624c413784e030868eb2503371e
김민혜 <kmhkgh@naver.com>
2022-07-29 15:57:06 +0900

    Fix: css 전반적인 수정(notion 이슈관리 > css 수정중)

--- components/Organisms/MyPage/Archive/BlankArchiveListSection.tsx
+++ components/Organisms/MyPage/Archive/BlankArchiveListSection.tsx
@@ -3,19 +3,24 @@ import { Box, FlexBox } from 'components/Atoms';
 
 export default function BlankArchiveListSection() {
   return (
-    <FlexBox
-      position="fixed"
-      top="calc(50% - 10px)"
+    <Box
+      // position="fixed"
+      // top="calc(50% - 10px)"
+      // width="100%"
+      // textAlign="center"
+      // flexDirection="column"
+      // alignItems="center"
+      paddingTop="140px"
+      margin="0 auto"
+      max-width="200px"
       width="100%"
       textAlign="center"
-      flexDirection="column"
-      alignItems="center"
     >
       <Alert />
-      <Box margin="20px" fontSize="13px" lineHeight="20px">
+      <Box marginTop="20px" fontSize="13px" lineHeight="20px">
         my 아카이브가 비어있습니다. <br />
         다녀온 문화생활로 채워주세요.
       </Box>
-    </FlexBox>
+    </Box>
   );
 }
