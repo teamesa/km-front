f08b96e66caab44a27fea6c34595460026934cd4
김한솔 <kimhan0421@naver.com>
2022-07-30 10:11:17 +0900

    Fix: 상세페이지 디테일 more 버튼 삭제

--- components/Organisms/Detail/Description/Introduce.tsx
+++ components/Organisms/Detail/Description/Introduce.tsx
@@ -1,8 +1,6 @@
-import { css } from '@emotion/react';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 
-import { Box, Button, Span } from 'components/Atoms';
+import { Box } from 'components/Atoms';
 import InnerHTML from 'components/Molecules/InnerHTML';
 import theme from 'styles/theme';
 
@@ -10,14 +8,6 @@ interface IntroduceProps {
   data: { summary: string; photo: string[] };
 }
 export default function Introduce({ data }: IntroduceProps) {
-  const [showMore, setShowMore] = useState(false);
-
-  useEffect(() => {
-    if (data.summary?.length < 300 || data.summary === null) {
-      setShowMore(true);
-    }
-  }, [data.summary, data.summary?.length]);
-
   return (
     <Box
       color={theme.colors.black}
@@ -26,32 +16,9 @@ export default function Introduce({ data }: IntroduceProps) {
       textAlign="left"
       padding="40px 15px 0 0"
     >
-      {showMore ? (
-        <Box>
-          <InnerHTML data={data.summary} />
-        </Box>
-      ) : (
-        <Box>
-          <Box
-            overflow="hidden"
-            display="-webkit-box"
-            css={css`
-              text-overflow: ellipsis;
-              -webkit-line-clamp: 5;
-              -webkit-box-orient: vertical;
-            `}
-          >
-            <InnerHTML data={data.summary} />
-          </Box>
-          <Button
-            fontSize="13px"
-            color={theme.colors.orange}
-            onClick={() => setShowMore(!showMore)}
-          >
-            more
-          </Button>
-        </Box>
-      )}
+      <Box>
+        <InnerHTML data={data.summary} />
+      </Box>
       {data.photo?.map((item, index) => (
         <Box marginTop="20px" key={index}>
           <Image
