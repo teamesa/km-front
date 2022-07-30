88851566dcca15b5490ae2587defc7355cf9f21c
Jinsil Han <truth.h406@gmail.com>
2022-07-30 09:19:35 +0900

    💄 add: 디테일 팝업 닫기버튼 sgv 추가

---
+++ assets/mypage/CloseBtn.tsx
@@ -0,0 +1,30 @@
+export const CloseBtn = ({ width = '28', height = '28' }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 28 28"
+    >
+      <g transform="translate(-240 -750)">
+        <circle
+          data-name="타원 209"
+          cx="14"
+          cy="14"
+          r="14"
+          transform="translate(240 750)"
+          fill="#fff"
+          opacity=".4"
+        />
+        <path
+          data-name="btn_X"
+          d="m860 5-5 5 5-5-5-5 5 5 5-5-5 5 5 5z"
+          transform="translate(-606 759)"
+          fill="none"
+          stroke="#fff"
+          strokeWidth="1.4px"
+        />
+      </g>
+    </svg>
+  );
+};
