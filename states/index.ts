2ec37b6c5ff112a47e171b5fa8ad4e2315f41158
noahlee <azxca1731@gmail.com>
2022-08-07 01:33:43 +0900

    잘못 된거 머지

--- states/index.ts
+++ states/index.ts
@@ -3,9 +3,8 @@ import { ArchiveWirteState } from './archiveWirte';
 import { DetailState } from './detail';
 import ListState from './list';
 import MyArchiveListState from './myArchiveList';
-import ResponseState from './response';
-import { searchRequest } from './search-request';
 import PopupNameState from './popupName';
+import { searchRequest } from './search-request';
 import TestSate from './test';
 
 export {
