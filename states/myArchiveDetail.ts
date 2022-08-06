9dffef0ea66df870226527d872f5069f23717d58
Jinsil Han <truth.h406@gmail.com>
2022-08-07 02:06:21 +0900

    add : axio, api 호출 연결

--- states/myArchiveDetail.ts
+++ states/myArchiveDetail.ts
@@ -1,4 +1,9 @@
+import { AxiosResponse } from 'axios';
+import { atom, selector } from 'recoil';
+
 import { PresentationBadge } from './myArchiveList';
+
+import customAxios from 'utils/hooks/customAxios';
 export interface MyArchiveDetailProps {
   typeBadge: PresentationBadge;
   updatedAt: string;
@@ -15,3 +20,21 @@ export interface ArchiveDetailLinkInfos {
   title: string;
   link: string;
 }
+
+export const getMyArchiveDetail = async (archiveId: string) => {
+  const axios = customAxios();
+  const { data } = (await axios({
+    url: `/api/archive/detail/${archiveId}`,
+    method: 'GET',
+  })) as AxiosResponse<MyArchiveDetailProps>;
+
+  return data;
+};
+
+export default atom({
+  key: 'MyArchiveDetailState',
+  default: selector({
+    key: 'MyArchiveDetailState/default',
+    get: () => getMyArchiveDetail('596'),
+  }),
+});
