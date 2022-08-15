88427d8bb1423f9a51caabfa63d632edd6d22c14
noahlee <azxca1731@gmail.com>
2022-08-15 17:30:21 +0900

    예외 처리

---
+++ components/Organisms/Popup/ArchivePictureOverflowPopup.tsx
@@ -0,0 +1,50 @@
+import { useSetRecoilState } from 'recoil';
+
+import { Box, Button } from 'components/Atoms';
+import Popup from 'components/Molecules/Popup';
+import { ALERT_MESSAGE } from 'constants/alertMessage';
+import { POPUP_NAME } from 'constants/popupName';
+import { PopupNameState } from 'states';
+import theme from 'styles/theme';
+
+const ArchivePictureOverflowPopup = () => {
+  const setPopupName = useSetRecoilState(PopupNameState);
+
+  const handleClosePopup = () => {
+    setPopupName(POPUP_NAME.NULL);
+  };
+
+  return (
+    <Popup>
+      <Box
+        width="315px"
+        borderRadius="12px"
+        height="167px"
+        backgroundColor={theme.colors.white}
+      >
+        <Box padding="50px 40px">
+          <Box
+            fontSize="13px"
+            lineHeight="1.54"
+            textAlign="center"
+            color={theme.colors.black}
+          >
+            {ALERT_MESSAGE.ALERT.OVERFLOW_PICTURE.message}
+          </Box>
+        </Box>
+        <Button
+          height="50px"
+          width="100%"
+          fontSize="16px"
+          borderRadius="0 0 12px 12px"
+          backgroundColor={theme.colors.black}
+          onClick={handleClosePopup}
+        >
+          <Box color={theme.colors.white}>확인</Box>
+        </Button>
+      </Box>
+    </Popup>
+  );
+};
+
+export default ArchivePictureOverflowPopup;
