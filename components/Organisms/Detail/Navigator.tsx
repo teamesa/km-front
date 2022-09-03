a3316a8befb7bd2d73720c758f52963406754c2f
김한솔 <kimhan0421@naver.com>
2022-09-03 15:25:25 +0900

    fix: 깃이 꼬임

--- components/Organisms/Detail/Navigator.tsx
+++ components/Organisms/Detail/Navigator.tsx
@@ -1,112 +1,90 @@
-import TopTabView from 'components/Molecules/TopTabView';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useRecoilValue, useRecoilValueLoadable } from 'recoil';
+import { useState, useEffect } from 'react';
+import { useRecoilValueLoadable } from 'recoil';
 
-import { Box, Button, Tag } from 'components/Atoms';
-import DescriptionInfo from 'components/Molecules/DescriptionInfo';
-import InnerHTML from 'components/Molecules/InnerHTML';
-import BottomSheetHeader from 'components/Organisms/Detail/Description/BottomSheetHeader';
+import NavWish from 'assets/common/bottomTabNavigator/NavWish';
+import Share from 'assets/detail/Share';
+import { Box, Button, FlexBox } from 'components/Atoms';
+import { Z_INDEX } from 'constants/common';
 import { DetailState } from 'states';
+import { TGetSummary } from 'states/detail';
 import theme from 'styles/theme';
 
-export default function Description() {
+export default function Navigator() {
   const router = useRouter();
+  const [currentUrl, setCurrentUrl] = useState('');
+
   const { id } = router.query;
-  const { contents, state } = useRecoilValueLoadable(DetailState(Number(id)));
-  const { summary, tabViewData } = contents;
+  const state = useRecoilValueLoadable(DetailState(Number(id)));
+  const data: TGetSummary = state?.contents?.summary;
 
-  const generateLinkUrl = ({
-    title,
-    link,
-  }: {
-    title: string;
-    link: string;
-  }) => {
-    if (!link) {
-      return null;
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setCurrentUrl(window.location.href);
     }
-    return (
-      <Button paddingTop="8px">
-        <Link href={link}>
-          <a target="_blank" rel="noreferrer">
-            <Box color={theme.colors.gray99}>
-              {title} {'>'}
-            </Box>
-          </a>
-        </Link>
-      </Button>
-    );
-  };
+  }, []);
 
-  switch (state) {
-    case 'hasValue':
-      return (
-        <Box position="relative" top="390px">
-          <BottomSheetHeader />
+  return (
+    <Box
+      zIndex={Z_INDEX.SKY}
+      backgroundColor={theme.colors.black}
+      width="100%"
+      bottom="0px"
+      position="fixed"
+      padding="0 15px"
+    >
+      <FlexBox height="60px" alignItems="center" justifyContent="space-between">
+        <FlexBox>
+          <Button marginTop="5px">
+            <NavWish stroke={theme.colors.white} />
+          </Button>
           <Box
-            backgroundColor={theme.colors.white}
-            padding="0 15px 60px"
-            marginTop="-1px"
+            fontSize="10px"
+            fontWeight={500}
+            alignSelf="center"
+            color={theme.colors.white}
+            marginLeft="2px"
           >
-            <Tag border={`1px solid ${theme.colors.black}`}>
-              {summary?.type}
-            </Tag>
-            <Box
-              marginTop="14px"
-              fontSize="19px"
-              fontWeight="500"
-              lineHeight="26px"
-            >
-              {summary?.title}
-            </Box>
-            <Box marginTop="30px" marginBottom="60px">
-              <DescriptionInfo title="기간" description={summary?.term} />
-              <DescriptionInfo title="장소" description={summary?.place} />
-              <DescriptionInfo
-                title="입장료"
-                description={
-                  <>
-                    <Box>{summary?.feeType}</Box>
-                    <Box>
-                      {summary?.price ? (
-                        <InnerHTML data={summary?.price} />
-                      ) : null}
-                    </Box>
-                    {generateLinkUrl({
-                      title: '티켓 구매하기',
-                      link: `${summary?.ticketUrl}`,
-                    })}
-                  </>
-                }
-              />
-              <DescriptionInfo
-                title={summary?.time ? '시간' : ''}
-                description={
-                  <>
-                    <Box>
-                      <InnerHTML data={summary?.time} />
-                    </Box>
-                    {generateLinkUrl({
-                      title: '홈페이지 이동',
-                      link: `${summary?.homePageUrl}`,
-                    })}
-                  </>
-                }
-              />
-            </Box>
-            <TopTabView
-              data={tabViewData.map((item: { title: any; contents: any }) => ({
-                title: item.title,
-                contents: item.contents,
-              }))}
-            />
+            {data?.itemInfoAdditionalInfo?.heartCount}
           </Box>
-        </Box>
-      );
-    case 'loading':
-      return <div>Loading...</div>;
-    case 'hasError':
-      throw <div>Error....</div>;
-  }
+          <Button
+            marginLeft="20px"
+            marginTop="5px"
+            onClick={() => {
+              navigator.clipboard
+                .writeText(currentUrl)
+                .then(() => {
+                  alert('클립보드에 복사되었습니다.');
+                })
+                .catch(() => {
+                  alert('주소복사에 실패했습니다');
+                });
+            }}
+          >
+            <Share />
+          </Button>
+        </FlexBox>
+        <Button
+          fontSize="16px"
+          fontWeight={500}
+          textAlign="left"
+          color={theme.colors.lime}
+          onClick={() => {
+            router.push({
+              pathname: '/archive',
+              query: {
+                id: id,
+                title: data?.title,
+                thumbnailImageUrl: data?.listImageUrl,
+                checked: true,
+              },
+            });
+          }}
+        >
+          아카이브 기록하기
+        </Button>
+      </FlexBox>
+      <Box width="100%" height="var(--platformBottomArea)" />
+    </Box>
+  );
 }
