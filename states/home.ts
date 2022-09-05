463a1e8c8a4ceff37fbe20948c50b6cf2e268bd4
noahlee <azxca1731@gmail.com>
2022-09-06 01:24:43 +0900

    목업 데이터 귀엽게 꾸미기

--- states/home.ts
+++ states/home.ts
@@ -14,20 +14,18 @@ export const getHomeInfo = async (): Promise<ModuleData[]> => {
       moduleName: 'swipe-item',
       data: {
         thumbnailPhotoUrl:
-          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-09-01/213104-8934b4282f1f93269a2b79cf929167b8_skdf.png',
+          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-07-24/125415-0bea704056e58f6195dd80ef18b09d3a_VOCpostergiorgiko.jpg',
         photoUrls: [
-          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-09-01/213104-8934b4282f1f93269a2b79cf929167b8_skdf.png',
-          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-09-01/213104-8934b4282f1f93269a2b79cf929167b8_skdf.png',
-          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-09-01/213104-8934b4282f1f93269a2b79cf929167b8_skdf.png',
-          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-09-01/213104-8934b4282f1f93269a2b79cf929167b8_skdf.png',
+          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-07-24/125547-11.jpg',
+          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-07-24/125549-12.jpg',
         ],
         title: {
-          text: '영국 현대미술의 거장 : 마이클 크레이그 마틴展 영국 현대미술의 거장 : 마이클 크레이그 마틴展',
-          link: '/detail/1099',
+          text: 'VANGUARDS OF THE COSMOS : GIORGIKO x MOONASSI',
+          link: '/detail/647',
         },
         content: {
-          text: '마이클 크레이그 마틴, 선과 색으로 개념을그리다. 마이클 크레이그 마틴의 초기작 ‘참나무 (An Oak Tree 1973)’ 작품은 변기를 미술전에 출품한 마르셀 뒤샹의 바톤을 이어받아, 당시 미술계에 파격적인이슈가 됩니다. 갤러리 벽면에 ‘선반과 물 한 잔을 올려놓고 물컵이 아닌... 참나무라고명명한이작품은 개념미술사의중요한전환점이됩니다.',
-          link: '/detail/1099',
+          text: '이달의 추천 전시회, 같이 보러 갈래? 에브리데이 몬데이에서 열리는 이번 듀엣전을 통해 두 작가는 각자의 세계에서 우주라는 공통된 주제로 무한의 개념을 유한의 예술 작업으로 표현하고자 한다.우주의 선구자들’은 미지의 공간으로 모험하는 것에 관한 것이다. "코스모"는 우리가 살고 있는 물리적 그리고 은유적 우주 모두를 말하고, "선봉자들"은 미지의 영역으로 가는 길을 이끄는 사람들이다.',
+          link: '/detail/647',
         },
         keywords: ['#전시회', '#한가람미술관'],
       },
@@ -36,22 +34,22 @@ export const getHomeInfo = async (): Promise<ModuleData[]> => {
       moduleName: 'swipe-item',
       data: {
         thumbnailPhotoUrl:
-          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-09-01/213104-8934b4282f1f93269a2b79cf929167b8_skdf.png',
+          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-06-27/122816-KakaoTalk_20211024_201607908_02.jpg',
         photoUrls: [
-          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-09-01/213104-8934b4282f1f93269a2b79cf929167b8_skdf.png',
-          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-09-01/213104-8934b4282f1f93269a2b79cf929167b8_skdf.png',
-          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-09-01/213104-8934b4282f1f93269a2b79cf929167b8_skdf.png',
-          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-09-01/213104-8934b4282f1f93269a2b79cf929167b8_skdf.png',
+          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-06-27/122826-KakaoTalk_20211024_201112747.jpg',
+          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-06-27/122829-KakaoTalk_20211024_201112747_01.jpg',
+          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-06-27/122833-KakaoTalk_20211024_201112747_02.jpg',
+          'https://kilometer-image.s3.ap-northeast-2.amazonaws.com/static/bo/2022-06-27/122839-KakaoTalk_20220408_153706828_04.jpg',
         ],
         title: {
-          text: '영국 현대미술의 거장 : 마이클 크레이그 마틴展 영국 현대미술의 거장 : 마이클 크레이그 마틴展',
-          link: '/detail/1099',
+          text: '사심을 가득채운 우리집 고양이 콘서트',
+          link: '/detail/516',
         },
         content: {
-          text: '마이클 크레이그 마틴, 선과 색으로 개념을그리다. 마이클 크레이그 마틴의 초기작 ‘참나무 (An Oak Tree 1973)’ 작품은 변기를 미술전에 출품한 마르셀 뒤샹의 바톤을 이어받아, 당시 미술계에 파격적인이슈가 됩니다. 갤러리 벽면에 ‘선반과 물 한 잔을 올려놓고 물컵이 아닌... 참나무라고명명한이작품은 개념미술사의중요한전환점이됩니다.',
-          link: '/detail/1099',
+          text: '미오 그 고양이의 귀여움은 어디까지 인가? 이 고양이는 너무 귀엽습니다. 모두들 보면 빠질 정도죠. 그래서 미오네집에서 미오의 놀이시간 콘서트를 개최합니다. 우아한 몸짓에 흠뻑 취하는 자신을 발견하실 수 있으실 겁니다.',
+          link: '/detail/516',
         },
-        keywords: ['#전시회', '#한가람미술관'],
+        keywords: ['#전시회', '#미오네집'],
       },
     },
     {
