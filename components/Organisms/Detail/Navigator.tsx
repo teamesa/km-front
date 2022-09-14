84d38105b34db61de2edb3e3e03d520af9102ee0
김한솔 <kimhan0421@naver.com>
2022-09-14 14:11:18 +0900

    Feat: 비로그인시 로그인 팝업띄우기

--- components/Organisms/Detail/Navigator.tsx
+++ components/Organisms/Detail/Navigator.tsx
@@ -1,27 +1,67 @@
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
-import { useRecoilValueLoadable } from 'recoil';
+import {
+  useRecoilValue,
+  useRecoilValueLoadable,
+  useSetRecoilState,
+} from 'recoil';
 
 import Share from 'assets/detail/Share';
 import { Box, Button, FlexBox } from 'components/Atoms';
 import NavigatorHeart from 'components/Organisms/Detail/NavigatorHeart';
+import { ALERT_MESSAGE } from 'constants/alertMessage';
 import { Z_INDEX } from 'constants/common';
-import { DetailState } from 'states';
+import { POPUP_NAME } from 'constants/popupName';
+import { AlertState, DetailState, PopupNameState } from 'states';
 import { TGetSummary } from 'states/detail';
+import { User } from 'states/user';
 import theme from 'styles/theme';
 
 export default function Navigator() {
   const router = useRouter();
   const [currentUrl, setCurrentUrl] = useState('');
+  const setAlertState = useSetRecoilState(AlertState);
+  const setPopupName = useSetRecoilState(PopupNameState);
   const { id } = router.query;
   const { contents, state } = useRecoilValueLoadable(DetailState(Number(id)));
   const data: TGetSummary = contents?.summary;
+  const userId = useRecoilValue(User).id;
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       setCurrentUrl(window.location.href);
     }
   }, []);
+
+  const clipboard = () => {
+    navigator.clipboard
+      .writeText(currentUrl)
+      .then(() => {
+        setAlertState(ALERT_MESSAGE.ALERT.COPY_TO_CLIPBOARD);
+        setPopupName(POPUP_NAME.ALERT_CONFIRM);
+      })
+      .catch(() => {
+        setAlertState(ALERT_MESSAGE.ERROR.ARCHIVE_REGISTRATION_QUESTION);
+        setPopupName(POPUP_NAME.ALERT_CONFIRM);
+      });
+  };
+
+  const archiveLink = () => {
+    if (userId < 0) {
+      setAlertState(ALERT_MESSAGE.ALERT.ASK_LOGIN);
+      setPopupName(POPUP_NAME.ALERT_LOGIN_CONFIRMATION);
+      return null;
+    }
+    router.push({
+      pathname: data?.itemInfoAdditionalInfo?.archiveLink.link,
+      query: {
+        id: id,
+        title: data?.title,
+        thumbnailImageUrl: data?.listImageUrl,
+        checked: true,
+      },
+    });
+  };
 
   switch (state) {
     case 'hasValue':
@@ -46,20 +86,7 @@ export default function Navigator() {
                   heartCount={data?.itemInfoAdditionalInfo?.heartCount}
                 />
               </Box>
-              <Button
-                marginLeft="20px"
-                marginTop="5px"
-                onClick={() => {
-                  navigator.clipboard
-                    .writeText(currentUrl)
-                    .then(() => {
-                      alert('클립보드에 복사되었습니다.');
-                    })
-                    .catch(() => {
-                      alert('주소복사에 실패했습니다');
-                    });
-                }}
-              >
+              <Button marginLeft="20px" marginTop="5px" onClick={clipboard}>
                 <Share />
               </Button>
             </FlexBox>
@@ -68,17 +95,7 @@ export default function Navigator() {
               fontWeight={500}
               textAlign="left"
               color={theme.colors.lime}
-              onClick={() => {
-                router.push({
-                  pathname: data?.itemInfoAdditionalInfo?.archiveLink.link,
-                  query: {
-                    id: id,
-                    title: data?.title,
-                    thumbnailImageUrl: data?.listImageUrl,
-                    checked: true,
-                  },
-                });
-              }}
+              onClick={archiveLink}
             >
               {data?.itemInfoAdditionalInfo?.archiveLink.title}
             </Button>
--- components/Organisms/Detail/NavigatorHeart.tsx
+++ components/Organisms/Detail/NavigatorHeart.tsx
@@ -1,11 +1,16 @@
 import { AxiosResponse } from 'axios';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import NavWish from 'assets/common/bottomTabNavigator/NavWish';
 import { Button, FlexBox, Span } from 'components/Atoms';
+import { ALERT_MESSAGE } from 'constants/alertMessage';
+import { POPUP_NAME } from 'constants/popupName';
+import { AlertState, PopupNameState } from 'states';
 import { getSummary } from 'states/detail';
 import { PresentationHeart } from 'states/list';
+import { User } from 'states/user';
 import theme from 'styles/theme';
 import customAxios from 'utils/hooks/customAxios';
 
@@ -24,8 +29,16 @@ export default function NavigatorHeart({
   const { id } = router.query;
   const [heartState, setHeartState] = useState(heartCount);
   const [click, setClick] = useState(heart.heartClicked);
+  const setAlertState = useSetRecoilState(AlertState);
+  const setPopupName = useSetRecoilState(PopupNameState);
+  const userId = useRecoilValue(User).id;
 
   const setToPick = async () => {
+    if (userId < 0) {
+      setAlertState(ALERT_MESSAGE.ALERT.ASK_LOGIN);
+      setPopupName(POPUP_NAME.ALERT_LOGIN_CONFIRMATION);
+      return null;
+    }
     const axios = customAxios();
     const url = `${heart.link}${!click}`;
 
