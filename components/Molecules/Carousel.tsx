b5a7c28add9a9766df7010456d6f522a395823cf
Jinsil Han <truth.h406@gmail.com>
2022-09-17 18:03:36 +0900

    delete: 캐러셀 기능 새로 작성 위해 코드 삭제

--- components/Molecules/Carousel.tsx
+++ components/Molecules/Carousel.tsx
@@ -5,57 +5,7 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import { FlexBox, Box } from 'components/Atoms';
 import theme from 'styles/theme';
 
-export default function Carousel({
-  itemsPerSlide,
-  imgUrlArr,
-}: {
-  itemsPerSlide: number;
-  imgUrlArr: string[];
-}) {
-  const itemsRef = useRef<any>(null);
-  const scrollRef = useRef<any>(null);
-
-  // 현재 이미지 인덱스 값. 첫시작 : 1
-  const [numberOfIndicators, setNumberOfItems] = useState(1);
-  // 범위 계산하기
-
-  let indexScrollPositionArr: number[] = [];
-
-  const calculateIndicatorDimensions = useCallback(() => {
-    const itemWidth = Math.ceil(itemsRef.current.offsetWidth / itemsPerSlide);
-    let indexScrollPosition = 0;
-
-    for (let i = 0; i < itemsPerSlide; i++) {
-      indexScrollPositionArr.push(indexScrollPosition);
-      indexScrollPosition += itemWidth;
-    }
-    setNumberOfItems(numberOfIndicators);
-  }, [indexScrollPositionArr, itemsPerSlide, numberOfIndicators]);
-
-  // 화면 리사이즈되면 width 이런거 다시 계산
-  useEffect(() => {
-    window.addEventListener('resize', calculateIndicatorDimensions);
-    return () => {
-      window.removeEventListener('resize', calculateIndicatorDimensions);
-    };
-  }, [calculateIndicatorDimensions]);
-
-  useEffect(() => {
-    calculateIndicatorDimensions();
-  }, [calculateIndicatorDimensions]);
-
-  // scroll 이벤트 멈췄을때 scrollTop 이 어디 범위(인덱스에) 있는지 판단해서
-  // 인디케이터 useState 하기.
-
-  const handleIndicator = () => {
-    const index = indexScrollPositionArr.indexOf(
-      Math.ceil(scrollRef.current.scrollLeft),
-    );
-    if (index + 1 > 0) {
-      setNumberOfItems(index + 1);
-    }
-  };
-
+export default function Carousel({}: {}) {
   return (
     <Box position="relative">
       {imgUrlArr.length > 1 ? (
@@ -71,27 +21,19 @@ export default function Carousel({
           right="15px"
           zIndex="100"
           color={theme.colors.white}
-        >
-          {numberOfIndicators} / {itemsPerSlide}
-        </Box>
+        ></Box>
       ) : null}
       <Box
         width="345px"
         height="345px"
         overflowY="hidden"
         overflowX={imgUrlArr.length < 2 ? 'hidden' : 'scroll'}
-        onScroll={handleIndicator}
         css={css`
           scroll-snap-type: x mandatory;
         `}
         ref={scrollRef}
       >
-        <FlexBox
-          width="max-content"
-          height="345px"
-          flexDirection="row"
-          ref={itemsRef}
-        >
+        <FlexBox width="max-content" height="345px" flexDirection="row">
           {imgUrlArr.length > 0 ? (
             imgUrlArr.map((imgUrl, _index) => (
               <Box
