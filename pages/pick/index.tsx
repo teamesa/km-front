b6a9ae4dbb43a8386025c686570f3166c4e1316d
noahlee <azxca1731@gmail.com>
2022-09-24 08:32:06 +0900

    build fix

--- pages/pick/index.tsx
+++ pages/pick/index.tsx
@@ -1,22 +1,15 @@
 import { NextPage } from 'next';
 import { useEffect } from 'react';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 
 import MyPickPage from 'components/Organisms/Pick/MyPickPage';
 import PickLogin from 'components/Organisms/Pick/PickLogin';
-import { useResetPickStateFunction } from 'states/pick';
 import { User } from 'states/user';
 import { UserProps, useUserProps } from 'utils/authentication/useUser';
 import { useInitHeader } from 'utils/hooks/useInitHeader';
 
-export default function Pick() {
-  const loginState = useRecoilValue(User);
-  const resetPickState = useResetPickStateFunction();
-
-  useEffect(() => {
-    resetPickState();
-  }, [resetPickState]);
-
+const Pick: NextPage<UserProps> = ({ user }) => {
+  const setUserFirst = useSetRecoilState(User);
 
   useInitHeader({
     headerRight: 'search',
--- states/index.ts
+++ states/index.ts
@@ -3,11 +3,11 @@ import { ArchiveWriteState } from './archiveWirte';
 import { DetailState } from './detail';
 import MyArchiveListState from './myArchiveList';
 import PopupNameState from './popupName';
+
 import { searchRequest } from 'states/search-request';
 
 export {
   DetailState,
-  ListState,
   ArchiveWriteState,
   searchRequest,
   MyArchiveListState,
