947baef26e847684b483bc534715921dbedef572
Kim Han Sol <48988891+kimhan0421@users.noreply.github.com>
2022-10-27 21:37:31 +0900

    상세페이지 관련 에러처리 (#121)
    * Fix: 홈페이지 바로가기 수정
    
    * Fix: 안쓰는 함수 제거
    
    * Refactor: 돋보기와 홈 아이콘 간격조절
    
    * Refactor: 돋보기와 홈 아이콘 간격조절
    
    * Feat: 아카이브 제목 추가
    
    * Feat: 단순 아카이브 제목 추가
    
    * Feat: 아카이브 스크롤 paddingtop 추가
    
    * Refacotr: detail state 재정비 및 summary 파일 나누기
    
    * Fix: 아카이브 새로고침
    
    * Feat: 사진 아카이브만 눌렀을 때 사진 아카이브만 노출

--- pages/detail/[id]/index.tsx
+++ pages/detail/[id]/index.tsx
@@ -1,12 +1,20 @@
+import { useEffect } from 'react';
+
 import { Box } from 'components/Atoms';
 import Description from 'components/Organisms/Detail/Description';
 import ExhibitionImagesSection from 'components/Organisms/Detail/ExhibitionImagesSection';
 import Navigator from 'components/Organisms/Detail/Navigator';
+import { useResetDetailArchiveFunction } from 'states/detail';
 import theme from 'styles/theme';
 import { useInitHeader } from 'utils/hooks/useInitHeader';
 
 export default function Detail() {
   useInitHeader({ headerLeft: 'default', headerEnd: 'home' });
+  const resetArchiveState = useResetDetailArchiveFunction();
+
+  useEffect(() => {
+    resetArchiveState();
+  }, [resetArchiveState]);
 
   return (
     <Box backgroundColor={theme.colors.grayEE}>
