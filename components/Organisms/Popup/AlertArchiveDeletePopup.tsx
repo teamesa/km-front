f76d0a4665e2bce53d5dd4087644abe391525816
Jinsil Han <truth.h406@gmail.com>
2022-11-06 11:35:55 +0900

    add: 삭제 기능 진행중 ..

--- components/Organisms/Popup/AlertArchiveDeletePopup.tsx
+++ components/Organisms/Popup/AlertArchiveDeletePopup.tsx
@@ -1,23 +1,45 @@
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { Box, Button } from 'components/Atoms';
 import Popup from 'components/Molecules/Popup';
 import { POPUP_NAME } from 'constants/popupName';
 import { AlertState, PopupNameState } from 'states';
-import { ClickedArchiveId } from 'states/myArchiveDetail';
+import {
+  ClickedArchiveId,
+  myArchiveDeleteResponse,
+} from 'states/myArchiveDetail';
 import theme from 'styles/theme';
+import customAxios from 'utils/hooks/customAxios';
 
 /** TODO: 취소 및 확인 누른 후 액션에 대한 정의필요 */
 
 const AlertArchiveDeletePopup = () => {
   const alertState = useRecoilValue(AlertState);
   const setPopupName = useSetRecoilState(PopupNameState);
-
   const clickedArchiveIdState = useRecoilValue(ClickedArchiveId);
 
-  const handleConfirm = () => {
-    setPopupName(POPUP_NAME.NULL);
-    console.log(clickedArchiveIdState);
+  const [response, setResponse] = useState('');
+  const [readStatus, writeStatus] = useState('');
+
+  const router = useRouter();
+
+  const dd = async () => {
+    const axios = customAxios();
+    const { data } = await axios({
+      url: `/api/archive/${clickedArchiveIdState.toString()}`,
+      method: 'DELETE',
+    });
+
+    return data;
+  };
+
+  const aaa = () => {
+    dd().then(() => {
+      setResponse('success');
+    });
   };
 
   const handleCancel = () => {
@@ -58,7 +80,7 @@ const AlertArchiveDeletePopup = () => {
           fontSize="16px"
           borderRadius="0 0 12px 0"
           backgroundColor={theme.colors.black}
-          onClick={handleConfirm}
+          onClick={aaa}
         >
           <Box color={theme.colors.white}>확인</Box>
         </Button>
