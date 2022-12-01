626503d6d2cb32d8a3449bf83d039685e19eddf2
김한솔 <kimhan0421@naver.com>
2022-12-01 20:38:09 +0900

    Feat: archive 진입시 비로그인 되면 mypage로 이동

--- pages/archive/index.tsx
+++ pages/archive/index.tsx
@@ -1,12 +1,28 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
-import { Box, Layout } from 'components/Atoms';
+import { Box } from 'components/Atoms';
 import ArchiveHome from 'components/Organisms/Archive/ArchiveHome';
+import { ALERT_MESSAGE } from 'constants/alertMessage';
+import { POPUP_NAME } from 'constants/popupName';
+import { AlertState, PopupNameState } from 'states';
+import { User } from 'states/user';
 import theme from 'styles/theme';
 import { useInitHeader } from 'utils/hooks/useInitHeader';
 
 export default function Archive() {
   const router = useRouter();
+  const loginState = useRecoilValue(User);
+  const setAlertState = useSetRecoilState(AlertState);
+  const setPopupName = useSetRecoilState(PopupNameState);
+
+  useEffect(() => {
+    if (!loginState.isLogin) {
+      setAlertState(ALERT_MESSAGE.ALERT.LOGIN_CONFIRMATION);
+      setPopupName(POPUP_NAME.ALERT_LOGIN_CONFIRMATION);
+    }
+  }, [loginState, loginState.isLogin, router, setAlertState, setPopupName]);
 
   useInitHeader({
     headerLeft: 'default',
