4a154a287de5d32e97f2766d6bf4ac27984f07af
한정아 <32234327+truthone@users.noreply.github.com>
2022-12-02 18:05:14 +0900

    update: 마이아카이브 > 아카이브 추가 (플로팅 버튼) (#154)
    * Feat: 아카이브 수정하기 api
    
    * Feat: 아카이브 기록하기  title 분리
    
    * Refactor: 아카이브 기록하기, 수정하기 에서 Title 가져올 수 있도록 변경
    
    * Fix: 아카이브 수정하기에서 다른사람도 보여주기 값 가져오기
    
    * Fix: 아카이브 보여주기 데이터가져오고, 체크 선택해제 가능하게 하기
    
    * Fix: search title 줄 안맞음
    
    * Refactor: search popup_ 아이콘 적용, 글자라인 변경
    
    * add: 플로팅 버튼 추가
    
    * add: 아카이브 추가페이지 라우터 연결
    
    * update: router push 수정
    
    Co-authored-by: 김한솔 <kimhan0421@naver.com>

---
+++ components/Molecules/FloatingButton.tsx
@@ -0,0 +1,36 @@
+import { css } from '@emotion/react';
+import router from 'next/router';
+
+import { Folder } from 'assets/mypage/Folder';
+import { Box } from 'components/Atoms';
+
+export default function FloatingButton() {
+  return (
+    <Box
+      display="block"
+      flex-wrap="nowrap"
+      overflow="initial"
+      position="fixed"
+      bottom="80px"
+      marginBottom="var(--platformBottomArea)"
+      right="15px"
+      onClick={() => router.push('/archive')}
+    >
+      <Box position="absolute" zIndex="1000">
+        <Folder />
+      </Box>
+      <Box
+        width="50px"
+        height="50px"
+        borderRadius="50%"
+        backgroundColor="#000"
+        zIndex="-1"
+        css={css`
+          filter: blur(4px);
+          -webkit-filter: blur(4px);
+          opacity: 0.8;
+        `}
+      ></Box>
+    </Box>
+  );
+}
