ba76f4204f81348f6b73c628c729a9c8e4bebcd2
JungAh Han <truth.h406@gmail.com>
2022-12-18 11:19:17 +0900

    add: 코멘트 박스 css 수정중..

--- components/Organisms/MyPage/Archive/Detail/MyArchiveDetailCard.tsx
+++ components/Organisms/MyPage/Archive/Detail/MyArchiveDetailCard.tsx
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react';
 
-import { Box } from 'components/Atoms';
+import { Box, FlexBox } from 'components/Atoms';
 import Carousel from 'components/Molecules/Carousel';
 import InnerHTML from 'components/Molecules/InnerHTML';
 import NoItemBox from 'components/Molecules/NoItemBox';
@@ -11,57 +11,83 @@ import theme from 'styles/theme';
 
 export default function MyArchiveDetailCard(contents: MyArchiveDetailProps) {
   return (
-    <Box background="#fff" width="345px" height="652px" margin="15px">
+    <Box background="#fff" width="345px" margin="15px">
       <MyArchiveDetailHeaderInfo
         updatedAt={contents.updatedAt}
         typeBadge={contents.typeBadge}
         title={contents.title}
         archiveAdditionalInfos={contents.archiveAdditionalInfos}
       />
-      <Box
-        width="100%"
-        height="347px"
-        borderTop="solid 1px"
-        borderTopColor={theme.colors.gray99}
-        borderBottom="solid 1px"
-        borderBottomColor={theme.colors.gray99}
-      >
-        {contents.photoUrls.length === 0 ? (
-          <NoItemBox width="inherit" height="inherit" text="사진 " />
-        ) : (
-          <Carousel
-            imgUrlArr={contents.photoUrls}
-            width={'345px'}
-            height={'345px'}
-          />
-        )}
-      </Box>
-      <Box
-        height="88px"
-        margin="20px 5px"
-        overflowY="auto"
-        fontSize="12px"
-        textAlign="left"
-        lineHeight="18px"
-        css={css`
-          ::-webkit-scrollbar {
-            display: block;
-            width: 2px;
-            height: 100%;
-            background: gray;
-          }
-          ::-webkit-scrollbar-thumb {
-            background: #000;
-          }
-        `}
-      >
-        {contents.comment === '' ? (
-          <NoItemBox width="inherit" height="inherit" text="코멘트" />
-        ) : (
-          <Box width="inherit" height="fit-content" margin="0 10px">
-            <InnerHTML data={contents.comment} />
+      <Box position="relative">
+        <Box
+          width="100%"
+          height="347px"
+          borderTop="solid 1px"
+          borderTopColor={theme.colors.gray99}
+          borderBottom="solid 1px"
+          borderBottomColor={theme.colors.gray99}
+        >
+          {contents.photoUrls.length === 0 ? (
+            <NoItemBox width="inherit" height="inherit" text="사진 " />
+          ) : (
+            <Carousel
+              imgUrlArr={contents.photoUrls}
+              width={'345px'}
+              height={'345px'}
+            />
+          )}
+        </Box>
+        <FlexBox
+          width="100%"
+          height="118px"
+          position="absolute"
+          bottom="10px"
+          justifyContent="center"
+          alignItems="center"
+        >
+          {/* dim */}
+          <Box
+            position="absolute"
+            width="325px"
+            height="118px"
+            backgroundColor={theme.colors.black}
+            opacity={0.4}
+          ></Box>
+          <Box
+            width="288px"
+            height="118px"
+            marginRight="15px"
+            overflowY="auto"
+            lineHeight="18px"
+            zIndex="1000"
+            css={css`
+              ::-webkit-scrollbar {
+                display: block;
+                width: 2px;
+                height: 100%;
+                background: white;
+              }
+              ::-webkit-scrollbar-thumb {
+                background: gray;
+              }
+            `}
+          >
+            {contents.comment === '' ? (
+              <NoItemBox width="inherit" height="inherit" text="코멘트" />
+            ) : (
+              <Box
+                width="inherit"
+                height="118px"
+                paddingRight="15px"
+                fontSize="12px"
+                textAlign="left"
+                color={theme.colors.white}
+              >
+                <InnerHTML data={contents.comment} />
+              </Box>
+            )}
           </Box>
-        )}
+        </FlexBox>
       </Box>
       <MyArchiveDetailCardInfo
         starRating={contents.starRating}
