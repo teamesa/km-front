e906734fdb5af71ac34042efa4ee7f72d05c6c52
JungAh Han <truth.h406@gmail.com>
2023-01-15 15:24:09 +0900

    update: 아카이브 없음 안내 화면 css 수정

--- components/Organisms/MyPage/Archive/ListSection.tsx
+++ components/Organisms/MyPage/Archive/ListSection.tsx
@@ -1,16 +1,21 @@
 import ListCard from './ListCard';
 
 import { Box } from 'components/Atoms';
+import MyArchiveListFragment from 'components/Organisms/MyPage/Archive/MyArchiveListFragment';
 import { MyArchivePageContents } from 'states/myArchiveList';
 
 export default function ListSection(data: any) {
-  return (
-    <Box paddingBottom="60px">
-      {data?.contents?.contents?.map(
-        (content: MyArchivePageContents, index: number) => (
-          <ListCard key={content?.api ?? index} content={content} />
-        ),
-      )}
-    </Box>
-  );
+  if (data.contents.length === 0) {
+    return <MyArchiveListFragment />;
+  } else {
+    return (
+      <Box paddingBottom="60px">
+        {data?.contents?.contents?.map(
+          (content: MyArchivePageContents, index: number) => (
+            <ListCard key={content?.api ?? index} content={content} />
+          ),
+        )}
+      </Box>
+    );
+  }
 }
