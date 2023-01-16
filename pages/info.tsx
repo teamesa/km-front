2cffb56fbe9d0939dc19043b8d439ca363f5c386
김한솔 <kimhan0421@naver.com>
2023-01-17 01:45:40 +0900

    Feat: 킬로미터 소개 page, logo 변경

--- pages/info.tsx
+++ pages/info.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 
 import { Box } from 'components/Atoms';
+import Introdeuce from 'components/Organisms/Info/Introduce';
 import PrivacyTerms from 'components/Organisms/Info/PrivacyTerms';
 import ServiceTerms from 'components/Organisms/Info/ServiceTerms';
 import { useInitHeader } from 'utils/hooks/useInitHeader';
@@ -9,6 +10,7 @@ const getInfoType = (type: string[] | string | undefined) => {
   switch (type) {
     case 'privacy':
     case 'service':
+    case 'introduce':
       return type;
     default:
       return 'disabled';
@@ -18,15 +20,27 @@ const getInfoType = (type: string[] | string | undefined) => {
 export default function Info() {
   const router = useRouter();
   const { type } = router.query;
+
   useInitHeader({
     headerLeft: getInfoType(type),
     headerRight: 'close',
     headerRightAction: () => router.back(),
   });
 
+  if (type === 'privacy') {
+    return (
+      <Box borderTop="1px solid #f3f3f3" paddingTop="30px" paddingX="15px">
+        <PrivacyTerms />
+      </Box>
+    );
+  } else if (type === 'service') {
+    <Box borderTop="1px solid #f3f3f3" paddingTop="30px" paddingX="15px">
+      <ServiceTerms />
+    </Box>;
+  }
   return (
     <Box borderTop="1px solid #f3f3f3" paddingTop="30px" paddingX="15px">
-      {getInfoType(type) === 'privacy' ? <PrivacyTerms /> : <ServiceTerms />}
+      <Introdeuce />
     </Box>
   );
 }
