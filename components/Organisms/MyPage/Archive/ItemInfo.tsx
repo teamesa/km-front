a92eaf92dc98e65baadc3544f08ca9b6eb4b55c4
Kim Han Sol <48988891+kimhan0421@users.noreply.github.com>
2023-01-29 11:53:20 +0900

    [Sprint32-2] home 화면, 마이페이지 화면 (#188)
    * [SPRINT32] connect with home api
    
    * [SPRINT32] remove unused console.log
    
    * [SPRINT32] add br tag for blank title
    
    * [SPRINT32] change typo file extention
    
    * [Refactor] 옵셔널체이닝 등 추가 및 수정 (#185)
    
    * add: 코멘트 박스 css 수정중..
    
    * add: 마이아카이브 디테일 팝업 코멘트 박스 디자인 수정
    
    * update: 사진, 코멘트 없음 안내 텍스트 박스 디자인 변경
    
    * update: 사진,코멘트, 다녀온곳 없는 경우 안내 텍스트 박스 css 수정"
    
    * update: 캐러셀영역, 사진 텍스트 박스 css 수정
    
    * 색상 추가 및 변경
    
    * update: 마이아카이브 팝업 헤더 css 수정
    
    * update: 캐러셀 인디케이터 css 수정
    
    * update: 수정 후 아카이브 디테일 내용 갱신 되도록 수정
    
    * update:아카이브 갯수 0일때 안내 화면 보여주기
    
    * update: 아카이브 없음 안내 화면 css 수정
    
    * update: 옵셔널체이닝
    
    * delete: 안쓰는 import, 오타수정
    
    * add: 마이 아카이브 리스트 하단 공백 조정
    
    * update: 커서 포인터 설정
    
    * update: 커서 포인터 추가 및 이미지와 카드내용 클릭영역 분리
    
    * delete: 주석삭제
    
    * delete: 안쓰는 코드 삭제
    
    * todo 써놓기
    
    * Refactor: 마이 아카이브 팝업
    
    * Refactor: 컴포넌트명 변경 및 안쓰는 코드 삭제
    
    * Refactor: ListCard style
    
    * Refactor: Listcard , ItemInfo
    
    * refactor: 변수 전부 앞에 선언
    
    * update: 프래그먼트 축약 변환
    
    * update: 데이터의존컴포넌트를  css 의존 뷰로직에서  비즈니스 로직으로
    
    * update: 커서 css 대신 button 태그로 바꾸고 영역 나누기
    
    * add: 스피너 로딩화면 추가
    
    * udpate: 옵셔널체이닝
    
    * update: 데이터 의존 컴포넌트 비즈니스로직으로 + 옵셔널 체이닝
    
    * Refactor: 충돌 해결
    
    * delete: 현재 필요하지 않은 코드
    
    * update: endPoint 변경됨에 따른 수정
    
    * update: 유실된 데이터-디테일팝업 디자인변경
    
    * updaet,delete: 필요없는 코드 삭제 및 revert
    
    * update: button 태그를 div + cursor 조합으로
    
    * add: 섬네일이미지 클릭시 해당 전시아이템 상세페이지 이동
    
    * update: 별점 css 수정중 ..
    
    * delete: 주석
    
    ---------
    
    Co-authored-by: 김한솔 <kimhan0421@naver.com>
    
    ---------
    
    Co-authored-by: noahlee <azxca1731@gmail.com>
    Co-authored-by: 한정아 <32234327+truthone@users.noreply.github.com>

--- components/Organisms/MyPage/Archive/ItemInfo.tsx
+++ components/Organisms/MyPage/Archive/ItemInfo.tsx
@@ -3,23 +3,20 @@ import { FlexBox, Span, Tag } from 'components/Atoms';
 import { PresentationBadge } from 'states/myArchiveList';
 import theme from 'styles/theme';
 
-type InfoProps = {
+export default function ItemInfo({
+  typeBadge,
+  updatedAt,
+  isMultiImages,
+}: {
   typeBadge: PresentationBadge;
-  presentationTitle: string;
   updatedAt: string;
   isMultiImages: boolean;
-};
-
-export default function ItemInfo(props: InfoProps) {
-  const typeBadge = props.typeBadge;
-  const updatedAt = props.updatedAt;
-  const isMultiImages = props.isMultiImages;
-
+}) {
   return (
     <FlexBox justifyContent="space-between">
       <FlexBox fontSize="11px" alignItems="center">
         <Tag backgroundColor={theme.colors.black} color={theme.colors.lime}>
-          {typeBadge.text}
+          {typeBadge?.text}
         </Tag>
         <Span marginLeft="5px" color={theme.colors.gray99}>
           {updatedAt}
