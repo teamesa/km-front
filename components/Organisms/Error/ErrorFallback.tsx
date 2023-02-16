773b31e0bec570913646e3ef86436d3c41d2070b
김한솔 <kimhan0421@naver.com>
2023-02-16 20:26:56 +0900

    Refactor: 문구수정

--- components/Organisms/Error/ErrorFallback.tsx
+++ components/Organisms/Error/ErrorFallback.tsx
@@ -18,7 +18,7 @@ export default function ErrorFallback() {
         lineHeight="20px"
         fontWeight="500"
       >
-        에러가 났습니다.
+        시스템 오류가 발생했습니다. 잠시 후 다시 시도해주세요.
       </Box>
     </Box>
   );
