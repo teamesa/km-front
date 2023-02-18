db1220c6fe4dcb66a338874e8c5d4f76b609ff10
김한솔 <kimhan0421@naver.com>
2023-02-18 21:43:59 +0900

    Feat: 상세페이지 useRecoilRefresher_UNSTABLE

--- pages/detail/[id]/index.tsx
+++ pages/detail/[id]/index.tsx
@@ -1,16 +1,14 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilRefresher_UNSTABLE, useSetRecoilState } from 'recoil';
 
 import { Box } from 'components/Atoms';
-import Description from 'components/Organisms/Detail/Description';
-import ExhibitionImagesSection from 'components/Organisms/Detail/ExhibitionImagesSection';
+import DescriptionContents from 'components/Organisms/Detail/DescriptionContents';
+import ExhibitionImage from 'components/Organisms/Detail/ExhibitionImage';
+import ItemInfo from 'components/Organisms/Detail/ItemInfo';
 import Navigator from 'components/Organisms/Detail/Navigator';
-import {
-  useResetDetailArchiveFunction,
-  useResetSummaryFunction,
-} from 'states/detail';
+import { detailState, useGetItemsById } from 'states/detail';
 import { User } from 'states/user';
 import theme from 'styles/theme';
 import { UserProps, useUserProps } from 'utils/authentication/useUser';
@@ -18,26 +16,41 @@ import { useInitHeader } from 'utils/hooks/useInitHeader';
 
 const Detail: NextPage<UserProps> = ({ user }) => {
   const router = useRouter();
+  const setUserFirst = useSetRecoilState(User);
+  const refreshGetItems = useRecoilRefresher_UNSTABLE(useGetItemsById);
+  const refreshDetailState = useRecoilRefresher_UNSTABLE(detailState);
+
   useInitHeader({
     headerLeft: 'default',
     headerEnd: 'home',
     headerLeftAction: () => router.back(),
   });
-  const setUserFirst = useSetRecoilState(User);
-  const resetArchiveState = useResetDetailArchiveFunction();
-  const resetPickState = useResetSummaryFunction();
 
   useEffect(() => {
     setUserFirst(user);
-    resetArchiveState();
-    resetPickState();
-  }, [resetArchiveState, resetPickState, setUserFirst, user]);
+    refreshGetItems();
+    refreshDetailState();
+  }, [refreshDetailState, refreshGetItems, setUserFirst, user]);
 
   return (
     <Box backgroundColor={theme.colors.grayEE}>
       <Box height="5px" />
-      <ExhibitionImagesSection />
-      <Description />
+      <ExhibitionImage />
+      <Box position="relative" top="390px">
+        <Box
+          height="25px"
+          backgroundColor={theme.colors.white}
+          borderRadius="24px 24px 0px 0px"
+        />
+        <Box
+          backgroundColor={theme.colors.white}
+          padding="0 15px 60px"
+          marginTop="-1px"
+        >
+          <ItemInfo />
+          <DescriptionContents />
+        </Box>
+      </Box>
       <Navigator />
     </Box>
   );
