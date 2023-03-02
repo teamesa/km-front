82cf2cb9f6570c474638477d34714348236015ae
김한솔 <kimhan0421@naver.com>
2023-03-03 01:25:29 +0900

    Fix: build error

--- states/detail.ts
+++ states/detail.ts
@@ -1,19 +1,86 @@
+import { AxiosResponse } from 'axios';
 import { selectorFamily } from 'recoil';
 
 import { getArchivesById } from 'api/v1/archive';
-import { getItmesDetailById, getItemsById } from 'api/v1/items';
+import { getItmesDetailById } from 'api/v1/items';
+import customAxios from 'utils/hooks/customAxios';
 
-export const useGetItemsById = selectorFamily({
-  key: 'UseGetItemsById',
-  get: (id: number) => async () => {
-    try {
-      const { data } = await getItemsById({ id: id });
-      return data;
-    } catch (error: any) {
-      console.error(error);
-    }
-  },
-});
+export type TGetSummary = {
+  detailImageUrl: string;
+  feeType: string;
+  homePageUrl: string;
+
+  itemInfoAdditionalInfo: {
+    archiveLink: {
+      title: string;
+      link: string;
+    };
+    heart: {
+      heartClicked: boolean;
+      id: number;
+      link: string;
+    };
+    heartCount: number;
+  };
+  lat: number;
+  listImageUrl: string;
+  lng: number;
+  place: string;
+  price: string;
+  term: string;
+  ticketUrl: string;
+  time: string;
+  title: string;
+  type: string;
+};
+
+export interface Archives {
+  id: number;
+  userProfileUrl: string;
+  userName: string;
+  updatedAt: string;
+  starRating: number;
+  likeCount: number;
+  heart: {
+    heartClicked: boolean;
+    link: string;
+  };
+  comment: string;
+  food: string;
+  cafe: string;
+  photoUrls: string[];
+}
+
+export interface ArchiveContents {
+  responsePagingStatus: {
+    nextPage: number;
+    currentPage: number;
+    pageSize: number;
+    hasNext: false;
+    totalContentsCount: number;
+    currentContentsCount: number;
+  };
+  avgStarRating: number;
+  archives: Archives[];
+}
+
+const axios = customAxios();
+
+const query = () => {
+  const pathname = window?.location?.pathname;
+  const queryData = pathname.slice(1).split('/');
+  return queryData;
+};
+
+export async function getSummary({ itemId }: { itemId?: number } = {}) {
+  const queryData = query();
+  const { data } = (await axios({
+    url: `/api/items/${itemId || Number(queryData[1])}`,
+    method: 'GET',
+  })) as AxiosResponse<TGetSummary>;
+
+  return data;
+}
 
 export const detailState = selectorFamily({
   key: 'DetailState',
