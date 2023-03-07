513d7567ca796890cc458f555f20aba4adc0ad24
Junghun Lee <azxca1731@gmail.com>
2023-03-08 00:04:48 +0900

    [SPRINT36] 아카이브 사진 useEffect (#235)

--- components/Organisms/ArchiveUpdate/ArchiveUpdateHome.tsx
+++ components/Organisms/ArchiveUpdate/ArchiveUpdateHome.tsx
@@ -39,7 +39,7 @@ export default function ArchiveUpdateHome() {
 
   useEffect(() => {
     setArchivePhotos(getSquareByUrls(getArchive?.photoUrls));
-  }, [setArchivePhotos]);
+  }, [getArchive]);
 
   const {
     register,
