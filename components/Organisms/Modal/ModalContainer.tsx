c8f9e0e8ba8c4516c0be89d0809aecc3b4cf4f0b
김민혜 <kmhkgh@naver.com>
2023-03-08 21:12:48 +0900

    Feat: 모달창 노출 후 페이지 이동시 닫히는 기능

--- components/Organisms/Modal/ModalContainer.tsx
+++ components/Organisms/Modal/ModalContainer.tsx
@@ -3,16 +3,28 @@ import { useRecoilValue, useSetRecoilState } from 'recoil';
 import ModalBox from 'components/Organisms/Modal/ModalBox';
 import { ModalComponents } from 'constants/type/initialModal';
 import { modalOutState, modalState } from 'states/modal';
+import { useModal } from 'utils/hooks/useModal';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function ModalContainer() {
   const modals = useRecoilValue(modalState);
   const setModalOutState = useSetRecoilState(modalOutState);
+  const router = useRouter();
+
+  const { offModal } = useModal();
 
   if (modals.length !== 0) {
     setModalOutState(true);
   } else {
     setModalOutState(false);
   }
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => {
+      offModal();
+    });
+  }, []);
 
   return (
     <>
