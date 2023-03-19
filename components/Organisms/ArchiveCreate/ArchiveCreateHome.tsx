1a15ddef52cb1ba3c93a5a77320cfd9bdd94bc8f
김한솔 <kimhan0421@naver.com>
2023-03-19 16:48:10 +0900

    [HOTFIX] 금칙어 적용

--- components/Organisms/ArchiveCreate/ArchiveCreateHome.tsx
+++ components/Organisms/ArchiveCreate/ArchiveCreateHome.tsx
@@ -80,7 +80,7 @@ export default function ArchiveCreateHome() {
       ),
     };
 
-    if (CheckForbiddenWords(customData.comment)) {
+    if (CheckForbiddenWords(data.comment)) {
       setAlertState(ALERT_MESSAGE.ALERT.FORBIDDEN_WORD);
       setPopupName(POPUP_NAME.FORBIDDEN_CONFIRM);
       return null;
